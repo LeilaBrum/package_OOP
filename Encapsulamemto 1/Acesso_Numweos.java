3944262ea6dfaf206d2aa0de5a3565f8a08e82ca
Leila Brum <leilabrum13@gmail.com>
2023-03-06 10:45:35 -0300

    criando

---
+++ Encapsulamemto 1/Acesso_Numweos.java
@@ -0,0 +1,26 @@
+package encapsulamento_1;
+
+public class Acesso_Numeros {
+	int a;
+	public int b;
+	private int  c;
+	protected int d;
+	
+	public void setNumero(String id, int numero) {
+		if (id == "a")
+			this.a = numero;
+		
+		if (id == "c")
+			this.c = numero;
+		
+		if (id == "d")
+			this.d = numero;
+	}
+	
+	public void mostra_numero() {
+		System.out.println("Número a = " + a);
+		System.out.println("Número b = " + b);
+		System.out.println("Número c = " + c);
+		System.out.println("Número d = " + d);
+	}
+}
